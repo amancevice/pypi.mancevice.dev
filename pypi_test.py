68a60112e84800974464013dc004c52296ec6242
Alexander Mancevice <amancevice@cargometrics.com>
2020-04-26 20:50:09 -0400

    py.test updates

--- pypi_test.py
+++ pypi_test.py
@@ -1,5 +1,6 @@
 import os
 
+import pytest
 import requests
 
 PYPI_HOST = os.getenv('PYPI_HOST')
@@ -8,11 +9,14 @@ PYPI_PASS = os.getenv('PYPI_PASS')
 PYPI_AUTH = requests.auth.HTTPBasicAuth(PYPI_USER, PYPI_PASS)
 
 
-def test_get_simple():
-    res = requests.get(f'https://{PYPI_HOST}/simple', auth=PYPI_AUTH)
-    assert res.status_code == 200
-
-
-def test_get_simple_pip():
-    res = requests.get(f'https://{PYPI_HOST}/simple/dip', auth=PYPI_AUTH)
-    assert res.status_code == 200
+@pytest.mark.parametrize(
+    ('url', 'status_code'),
+    [
+        (f'https://{PYPI_HOST}/simple', 200),
+        (f'https://{PYPI_HOST}/simple/dip', 200),
+        (f'https://{PYPI_HOST}/simple/boto3', 301),
+    ],
+)
+def test_GET(url, status_code):
+    res = requests.get(url, allow_redirects=False, auth=PYPI_AUTH)
+    assert res.status_code == status_code
