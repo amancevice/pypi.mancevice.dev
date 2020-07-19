027ce015643de10bd018d52c031bb823d84cc823
Alexander Mancevice <alexander.mancevice@hey.com>
2020-07-19 07:05:31 -0400

    terraform updates

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,9 @@
 variable ROLE_ARN { default = null }
 
 locals {
+  base_path       = "simple"
+  publish_lambdas = true
+
   tags = {
     App  = "pypi.mancevice.dev"
     Name = "mancevice.dev"
@@ -33,39 +36,48 @@ provider aws {
   }
 }
 
-# REST API
+# API GATEWAY :: REST API
 
 resource aws_api_gateway_rest_api pypi {
   description = "PyPI service"
   name        = "pypi.mancevice.dev"
-  # tags        = local.tags
+  tags        = local.tags
 
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource aws_api_gateway_stage simple {
-  cache_cluster_size = "0.5"
-  deployment_id      = aws_api_gateway_deployment.v1.id
-  description        = "Simple PyPI"
-  rest_api_id        = aws_api_gateway_rest_api.pypi.id
-  stage_name         = aws_api_gateway_deployment.v1.stage_name
-  tags               = local.tags
+resource aws_api_gateway_stage prod {
+  deployment_id = aws_api_gateway_deployment.v1.id
+  description   = "Simple PyPI"
+  rest_api_id   = aws_api_gateway_rest_api.pypi.id
+  stage_name    = "prod"
+  tags          = local.tags
 }
 
 resource aws_api_gateway_deployment v1 {
-  depends_on = []
-
   rest_api_id = aws_api_gateway_rest_api.pypi.id
-  stage_name  = "simple"
+
+  # depends_on = [
+  #   module.serverless_pypi.rest_api_integration_root_get,
+  #   module.serverless_pypi.rest_api_integration_root_head,
+  #   module.serverless_pypi.rest_api_integration_root_post,
+  #   module.serverless_pypi.rest_api_integration_proxy_get,
+  #   module.serverless_pypi.rest_api_integration_proxy_head,
+  #   module.serverless_pypi.rest_api_integration_proxy_post,
+  # ]
+
+  triggers = {
+    redeployment = module.serverless_pypi.rest_api_redeployment_trigger
+  }
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-# REST API DOMAIN
+# API GATEWAY :: DOMAIN
 
 data aws_acm_certificate mancevice_dev {
   domain = "mancevice.dev"
@@ -74,9 +86,9 @@ data aws_acm_certificate mancevice_dev {
 
 resource aws_api_gateway_base_path_mapping simple {
   api_id      = aws_api_gateway_rest_api.pypi.id
-  base_path   = "simple"
+  base_path   = local.base_path
   domain_name = aws_api_gateway_domain_name.pypi_mancevice_dev.domain_name
-  stage_name  = aws_api_gateway_stage.simple.stage_name
+  stage_name  = aws_api_gateway_stage.prod.stage_name
 }
 
 resource aws_api_gateway_domain_name pypi_mancevice_dev {
@@ -84,69 +96,47 @@ resource aws_api_gateway_domain_name pypi_mancevice_dev {
   domain_name     = aws_api_gateway_rest_api.pypi.name
 }
 
-# REST API AUTHORIZER
+# SERVERLESS PYPI
+
+module serverless_pypi {
+  source  = "amancevice/serverless-pypi/aws"
+  version = "~> 2.0"
+
+  iam_role_name  = "pypi-mancevice-dev"
+  s3_bucket_name = "pypi.mancevice.dev"
+  tags           = local.tags
+
+  lambda_api_fallback_index_url = "https://pypi.org/simple/"
+  lambda_api_function_name      = "pypi-mancevice-dev-api"
+  lambda_api_publish            = local.publish_lambdas
+
+  lambda_reindex_function_name = "pypi-mancevice-dev-reindex"
+  lambda_reindex_publish       = local.publish_lambdas
+
+  rest_api_authorization    = "CUSTOM"
+  rest_api_authorizer_id    = module.serverless_pypi_cognito.authorizer.id
+  rest_api_base_path        = local.base_path
+  rest_api_execution_arn    = aws_api_gateway_rest_api.pypi.execution_arn
+  rest_api_id               = aws_api_gateway_rest_api.pypi.id
+  rest_api_root_resource_id = aws_api_gateway_rest_api.pypi.root_resource_id
+}
+
+# SERVERLESS PYPI AUTHORIZER
 
 module serverless_pypi_cognito {
   source               = "amancevice/serverless-pypi-cognito/aws"
   version              = "~> 0.3"
   api_id               = aws_api_gateway_rest_api.pypi.id
   lambda_function_name = "pypi-mancevice-dev-authorizer"
-  lambda_publish       = true
+  lambda_publish       = local.publish_lambdas
   role_name            = "pypi-mancevice-dev-authorizer"
   tags                 = local.tags
   user_pool_name       = "pypi.mancevice.dev"
 }
 
-# SERVERLESS PYPI
-
-module serverless_pypi {
-  source                       = "amancevice/serverless-pypi/aws"
-  version                      = "~> 1.2"
-  api_authorization            = "CUSTOM"
-  api_authorizer_id            = module.serverless_pypi_cognito.authorizer.id
-  api_base_path                = aws_api_gateway_base_path_mapping.simple.base_path
-  api_name                     = "pypi.mancevice.dev"
-  fallback_index_url           = "https://pypi.org/simple/"
-  lambda_api_function_name     = "pypi-mancevice-dev-api"
-  lambda_api_publish           = true
-  lambda_reindex_function_name = "pypi-mancevice-dev-reindex"
-  lambda_reindex_publish       = true
-  role_name                    = "pypi-mancevice-dev"
-  s3_bucket_name               = "pypi.mancevice.dev"
-  tags                         = local.tags
-}
-
 # OUTPUTS
-
-output api_id {
-  description = "API Gateway REST API ID"
-  value       = aws_api_gateway_rest_api.pypi.id
-}
-
-output api_base_path {
-  description = "API Gateway Custom Domain base path"
-  value       = aws_api_gateway_base_path_mapping.simple.base_path
-}
-
-output cognito_client_id {
-  description = "Cognito user pool client ID"
-  value       = module.serverless_pypi_cognito.user_pool_client.id
-}
-
-output cognito_user_pool_id {
-  description = "Cognito user pool ID"
-  value       = module.serverless_pypi_cognito.user_pool.id
-}
-
-output lambda_api_arn {
-  value = module.serverless_pypi.lambda_api_arn
-}
-
-output lambda_reindex_arn {
-  value = module.serverless_pypi.lambda_reindex_arn
-}
 
 output pypi_url {
   description = "PyPI endpoint URL"
-  value       = "https://${aws_api_gateway_domain_name.pypi_mancevice_dev.domain_name}/${aws_api_gateway_base_path_mapping.simple.base_path}"
+  value       = "https://${aws_api_gateway_domain_name.pypi_mancevice_dev.domain_name}/${aws_api_gateway_base_path_mapping.simple.base_path}/"
 }
